a9b85919e98176553667887c0732e89e640bd7bf
antoniogmartin <antonio.gm011@gmail.com>
2026-01-19 14:41:58 +0100

    add test-data prefix

--- src/create_analysis_dataset.py
+++ src/create_analysis_dataset.py
@@ -62,7 +62,7 @@ def regex_find(folder_path,pattern, pattern2, analysis_name):
         suffix= parts[-1].replace('.fastq.gz', '').upper()
         sample_name = f"{parts[0]}-{suffix}".upper()
         #print(f"{sample_name}",f"jose-data/{file}",f"{suffix}",",,0")
-        matrix.append([sample_name,f"jose-data/{file}",None,suffix,None,"0"])
+        matrix.append([sample_name,f"test-data/jose-data/{file}",None,suffix,None,"0"])
     analysis_df = pd.DataFrame(matrix, columns=['Sample', "R1", "R2", "Condition", "Source", "Strandedness"])
     analysis_df.to_csv("../test-data/"+analysis_name, index=False)
 
